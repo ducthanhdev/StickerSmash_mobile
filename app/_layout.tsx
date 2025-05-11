9c611f2cea157186da4ccd8df044372f00aab947
ducthanhdev <nguyenvantho083@gmail.com>
2025-05-11 23:46:59 +0700

    Configure status bar, splash screen and app icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Home' }} />
-      <Stack.Screen name="about" options={{ title: 'About' }} />
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="about" options={{ title: "About" }} />
+      <StatusBar style="light" />
     </Stack>
   );
 }
